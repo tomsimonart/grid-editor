491c3db2081308f4674509f05ab63bba7edde5a1
elsoazemelet <elsoazemelet@gmail.com>
2024-11-15 13:43:01 +0100

    Fixed test typo

--- playwright-tests/pages/navbarPage.js
+++ playwright-tests/pages/navbarPage.js
@@ -2,7 +2,7 @@ export class NavbarPage {
   constructor(page) {
     this.page = page;
     this.configuration = page.getByTestId("nav-configuration");
-    this.preferences = page.getByTestId("nav-preferneces");
+    this.preferences = page.getByTestId("nav-preferences");
     this.profileCloud = page.getByTestId("nav-profile-cloud");
     this.debugMonitor = page.getByTestId("nav-debug-monitor");
     this.midiMonitor = page.getByTestId("nav-midi-monitor");
